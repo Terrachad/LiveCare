555f27f764473cc0864cfe283287ef8235b48afb
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-22 21:18:12 +0100

    config def

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -8,3 +8,11 @@ const {
 
 
 const client = new sdk.Client();
+
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API!)
+
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client)
+export const messaging = new sdk.Messaging(client)
+export const users = new sdk.Users(client);
