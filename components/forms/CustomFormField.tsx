288bbe9fc49075171581e5e10cd0e8a042c880f5
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-22 12:26:48 +0100

    Update CustomFormField.tsx
    todo: RenderField

--- components/forms/CustomFormField.tsx
+++ components/forms/CustomFormField.tsx
@@ -13,26 +13,45 @@ import { Control } from "react-hook-form"
 import { FormFieldType } from "./PatientForm"
 
 interface CustomFormFieldProps {
-    control: Control<any>,
+    control: Control<any>, // eslint-disable-line @typescript-eslint/no-explicit-any
     fieldType: FormFieldType,
+    name: string, 
+    label?: string,
+    placeholder?:string,
+    iconSrc?: string,
+    iconAlt?:string,
+    disabled?:boolean,
+    dateFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSkeleton?: (field:any) => React.ReactNode, // eslint-disable-line @typescript-eslint/no-explicit-any
 }
 
-const CustomFormField = ({control,fieldType, name}: CustomFormFieldProps) => {
-  return (
-    <FormField
-    control={control}
-    name={name}
-    render={({ field }) => (
-        <FormItem className="flex-1">
-            {fieldType !== FormFieldType.CHECKBOX && label (
-                <FormLabel>
-                    {label}
-                </FormLabel>
-            )}
-        </FormItem>
-    )}
-    />
-  )
+const RenderField = ({field, props}: {field:any, props: CustomFormFieldProps}) => { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return(
+        <Input
+        type="text"
+        placeholder="Giorgia Meloni"
+        />
+    )
+}
+
+const CustomFormField = (props: CustomFormFieldProps) => {
+    const {control, fieldType, name, label} = props;
+
+    return (
+        <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+            <FormItem className="flex-1">
+                {fieldType !== FormFieldType.CHECKBOX && label && (<FormLabel>{label}</FormLabel>)}
+                <RenderField field={field} props={props} />
+                <FormMessage className="shad-error"/>
+            </FormItem>
+        )}
+        />
+    )
 }
 
 export default CustomFormField
