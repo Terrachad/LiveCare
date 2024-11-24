a94c192f081ef53d04c56aace39da26e4095aee9
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 09:51:11 +0100

    Register page wrapper

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div
+    className='flex h-screen max-h-screen'
+    >
+        <section className='remove-scrollbar container'>
+
+        </section>
+    </div>
+  )
+}
+
+export default Register
