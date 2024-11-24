1285d61cfad34c91791bd786e78585a333ab5530
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 10:41:21 +0100

    export FormFieldType

---
+++ lib/enum.ts
@@ -0,0 +1,10 @@
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE = 'phone_input',
+    CHECKBOX = 'checkbox',
+    DATEPICKER = 'datepicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton',
+}
+ 
