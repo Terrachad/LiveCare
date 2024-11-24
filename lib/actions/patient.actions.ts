21eb51d6cfb16fa7d5b2bd44144efcb6644e69f9
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 09:51:34 +0100

    createUser handle

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,33 +1,35 @@
-import { Query } from "node-appwrite";
-import { users } from "../appwrite.config"
+"use server";
 
-// Define an interface for the Appwrite error structure
-interface AppwriteError extends Error {
-    code: number;
-    response?: {
-        message: string;
-        code: number;
-    };
-}
+import { ID, Query } from "node-appwrite";
 
+import {
+  users,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
-    try {
-        // user creation logic
+  try {
+    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+    const newuser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
 
-    } catch (error) {  // Removed the explicit type annotation
-        // Type guard to ensure error is AppwriteError
-        if (error && typeof error === 'object' && 'code' in error) {
-            const appwriteError = error as AppwriteError;
-            
-            if (appwriteError.code === 409) {
-                //Means user already exists 
-                const existingUserDocuments = await users.list([
-                    Query.equal('email', [user.email])
-                ])
-                return existingUserDocuments?.users[0]
-            }
-        }
+    return parseStringify(newuser);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    // Check existing user
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
 
-        console.log(`error while creating user ${error}`)
+      return existingUser.users[0];
     }
-}
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
