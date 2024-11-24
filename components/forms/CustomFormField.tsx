23ef365439995e6163b9e9b40ce1886cf1fc10dc
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 20:38:03 +0100

    CustomFormFields work finished
    FormFieldType.CHECKBOX 👍

--- components/forms/CustomFormField.tsx
+++ components/forms/CustomFormField.tsx
@@ -17,6 +17,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "../ui/select"
 import { Textarea } from "../ui/textarea"
+import { Checkbox } from "../ui/checkbox"
   
   interface CustomFormFieldProps {
       control: Control<any>,// eslint-disable-line @typescript-eslint/no-explicit-any
@@ -117,6 +118,24 @@ import { Textarea } from "../ui/textarea"
                                         {props.children}
                                     </SelectContent>
                                 </Select>
+                            </FormControl>
+
+                    )
+                break;
+                case FormFieldType.CHECKBOX:
+                    return (
+
+                            <FormControl>
+                                <div className="flex items-center gap-4">
+                                <Checkbox 
+                                id={props.name}
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                                />
+                                <label htmlFor={props.name} className="checkbox-label">
+                                    {props.label}
+                                </label>
+                                </div>
                             </FormControl>
 
                     )
