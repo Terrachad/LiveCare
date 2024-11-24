eb9aae8f49302570a0fa8d407ad83ccc0e64d275
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 03:08:18 +0100

    export env vars

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,6 +1,6 @@
 import * as sdk from 'node-appwrite'
 
-const {
+export const {
     PROJECT_ID, API, DOCTOR_COLLECTION_ID,LIVECARE_DB_ID, PATIENT_COLLECTION_ID,
     NEXT_PUBLIC_BUCKET_ID: BUCKET,
     NEXT_PUBLIC_ENDPOINT:ENDPOINT
