1367f37bd519142affb5070ab8f2a97d10a982bf
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-24 11:13:56 +0100

    Update page.tsx

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,7 +1,6 @@
 import RegisterForm from '@/components/forms/RegisterForm'
 import { getUser } from '@/lib/actions/patient.actions'
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
 
 const Register = async ({ params }: RegisterPageProps) => {
@@ -9,28 +8,20 @@ const Register = async ({ params }: RegisterPageProps) => {
     const user = await getUser(resolvedParams.userId)
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
-            src='/assets/icons/logo-full.svg'
+            src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
             alt="patient"
             className="mb-12 h-10 w-fit"
-          
           />
-            <RegisterForm user={user}/>
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-                © 2024 NETFINITY
-            </p>
-            <Link href='/?admin=true' className="text-green-500">
-              Admin?
-            </Link>
-          </div>
+          <RegisterForm user={user} />
+
+          <p className="copyright py-12">© 2024 NETFINITY</p>
         </div>
-
       </section>
       <Image
       src='/assets/images/register-img.png'
