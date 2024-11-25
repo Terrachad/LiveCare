65b047b916b35b4c71037df0bdd7b928176712be
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-25 10:15:22 +0100

    Change image import

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -32,7 +32,7 @@ export default async function NewAppointment({params: {userId}}: SearchParamProp
 
       </section>
       <Image
-      src='/assets/images/appointment.png'
+      src='/assets/images/appointment-img.png'
       height={1000}
       width={1000}
       alt="appointment"
