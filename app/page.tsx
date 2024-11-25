da31784454ccca0594366a3039b44e752e4faf83
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-25 14:34:18 +0100

    Update page.tsx
    check if admin

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import PatientForm from "@/components/forms/PatientForm"
 import Image from "next/image"
 import Link from "next/link"
  
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  // First, await the params
+  const isAdmin = searchParams.admin === 'true';
+  
+
   return (
     <div className="flex h-screen max-h-screen">
       {/** OTP */}
