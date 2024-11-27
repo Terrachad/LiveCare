8ea78d0a5c63688f0bb99a420d3548f4933c706e
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-27 23:34:20 +0100

    Update appointment.actions.ts
    add UpdateAppointment to cancel or schedule

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -4,6 +4,8 @@ import { ID, Query } from "node-appwrite"
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config"
 import { parseStringify } from "../utils"
 import { Appointment } from "@/types/appwrite.types"
+import { CreateAppointmentParams, UpdateAppointmentParams } from "@/types"
+import { revalidatePath } from "next/cache"
 
 export const createAppointment = async ( appointment : CreateAppointmentParams) =>{
     try {
@@ -75,3 +77,29 @@ export const getRecentAppointmentsList = async () =>{
     }
 }
 
+export const UpdateAppointment = async ( {appointmentId, userId, appointment, type}: UpdateAppointmentParams )=> {
+    try {
+        const updatedAppointment =  await databases.updateDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+            {
+                ...appointment,
+                userId, // Include userId in the update
+            }
+        )
+
+        if(!updatedAppointment){
+            throw new Error('Appointment not found')
+
+        }
+
+        //sms
+
+
+        revalidatePath('/admin')
+        return parseStringify(updatedAppointment)
+    } catch (error) {
+        console.log(error)
+    }
+}
