17bce478694d21a1b5b5b7821adeabe5a7f8de02
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-27 21:06:14 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 import {DataTable} from '@/components/DataTable'
 import StatCard from '@/components/StatCard'
-import { columns, Payment } from '@/components/table/colums'
+import { columns } from '@/components/table/colums'
 import { getRecentAppointmentsList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
