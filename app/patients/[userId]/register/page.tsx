6a97401a84901bba4295ea0b60c6b9bc2b5378f3
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-11-28 13:15:08 +0100

    fix: RegisterPageProps  import from types

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,5 +1,6 @@
 import RegisterForm from '@/components/forms/RegisterForm'
 import { getUser } from '@/lib/actions/patient.actions'
+import { RegisterPageProps } from '@/types'
 import Image from 'next/image'
 import React from 'react'
 
